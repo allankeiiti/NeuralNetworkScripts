d8de820c3208b21b31c3d8dea99565b73c3a7327
Allan <allan.keiiti@gmail.com>
2019-05-26 23:35:40 -0300

    Scripts das aulas de Redes Neurais Multicamadas

---
+++ Scripts/rede_multicamada.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def sigmoid(soma):
+    return 1 / (1 + np.exp(-soma))
+
+entradas = ([[0,0],
+             [0,1],
+             [1,0],
+             [1,1]])
+
+saidas = ([[0],[1],[1],[0]])
+
+pesos0 = ([[-0.424, -0.740, -0.961],
+           [0.358, -0.577, -0.469]])
+
+pesos1 = ([[-0.017], [-0.893], [0.148]])
+
+epocas = 100
+
+for j in range(epocas):
+    camadaEntrada = entradas
+    somaSinapse0 = np.dot(camadaEntrada, pesos0)
+    camadaOculta = sigmoid(somaSinapse0)
+
+
