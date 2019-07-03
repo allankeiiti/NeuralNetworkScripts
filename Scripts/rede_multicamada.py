a552902456d6567376957b7eaa28c3bc1d5ba350
Allan <allan.keiiti@gmail.com>
2019-07-02 22:20:10 -0300

    Implementação rede Multicamada IX e X - Colocado o atributo np.array na lista de entradas e saídas (Algo que esqueci da última vez)

--- Scripts/rede_multicamada.py
+++ Scripts/rede_multicamada.py
@@ -16,20 +16,28 @@ def sigmoid(soma):
 def sigmoidDerivada(slg):
     return slg * (1 - slg)
 
+
+
 #As entradas, saídas, pesos0, pesos1 e epocas foram retiradas da video-aula 25. Implementação rede multicamada I e II
-entradas = ([[0,0],
+entradas = np.array([[0,0],
              [0,1],
              [1,0],
              [1,1]])
 
-saidas = ([[0],[1],[1],[0]])
+saidas = np.array([[0],[1],[1],[0]])
 
-pesos0 = ([[-0.424, -0.740, -0.961],
-           [0.358, -0.577, -0.469]])
+# pesos0 = np.array([[-0.424, -0.740, -0.961],
+#           [0.358, -0.577, -0.469]])
 
-pesos1 = ([[-0.017], [-0.893], [0.148]])
+# pesos1 = np.array([[-0.017], [-0.893], [0.148]])
 
-epocas = 100
+#2 neurônios na camada de entrada e 3 neurônios na camada de saída
+pesos0 = 2 * np.random.random((2,3)) - 1
+pesos1 = 2 * np.random.random((3,1)) - 1
+
+epocas = 100000
+
+#Valores definidos pelo usuário
 taxaAprendizagem = 0.3
 momento = 1
 
@@ -43,9 +51,9 @@ for j in range(epocas):
     CamadaFinal = sigmoid(somaSinapse1)
 
     erroCamadaSaida = saidas - CamadaFinal
-
     #Media absoluta envolve os valores de Erro da camada de saída positivo. Verificar Imagem Rede_Neural_Multicamda_foto.PNG
     mediaAbsoluta = np.mean(np.abs(erroCamadaSaida))
+    print('Erro: ' + str(mediaAbsoluta))
 
     #Implementando o cálculo de DeltaSaida
     derivadaSaida = sigmoidDerivada(CamadaFinal)
@@ -56,16 +64,21 @@ for j in range(epocas):
     deltaSaidaXPeso = deltaSaida.dot(pesos1Transposta)
     deltaCamadaOculta =deltaSaidaXPeso * sigmoidDerivada(camadaOculta)
 
-    # Backpropagation
+    # Backpropagation 1
     camadaOcultaTransposta = camadaOculta.T
     pesosNovo1 = camadaOcultaTransposta.dot(deltaSaida)
     pesos1 = (pesos1 * momento) + (pesosNovo1 * taxaAprendizagem)
+
+    # Backpropagation 0
+    camadaEntradaTransposta = camadaEntrada.T
+    pesosNovo0 = camadaEntradaTransposta.dot(deltaCamadaOculta)
+    pesos0 = (pesos0 * momento) + (pesosNovo0 * taxaAprendizagem)
 
 # Lembrando, a função NumPy.DOT equivale às linhas abaixo, porém mais ágil:
 
 #DotProductcomFor(entradas, pesos):
 #    sum = 0
-#    for i in range(len(entradas)):
+#    for i in range(len(entradas66)):
 #        print(entradas[i])
 #        print(pesos[i])
 #        sum += entradas[i] * pesos[i]
